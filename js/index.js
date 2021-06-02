9f92c9c9b08c47b3ccaa14ac3f910373165c97d4
Claudia <claudiacachayosorio.web@gmail.com>
2021-06-02 01:08:52 -0400

    set event listener for state change

--- js/index.js
+++ js/index.js
@@ -1,32 +1,42 @@
-// Youtube Player
-
-// API call
+// Youtube API call
 const tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
 const firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-// Create player object
-let player;
-const playerParams = {};
 
-function onYouTubeIframeAPIReady() {
-	player = new YT.Player('player', playerParams);
-	console.log(playerParams);
-}
-
-
-// Darken main background
-
-const defaultArr = ['60', '1%', '68%'];
-
-function getHSL(arr) {
-	return `hsl(${arr[0]}, ${arr[1]}, ${arr[2]})`;
-}
-
+// Change background color
+const defaultBg = 'hsl(60, 1%, 68%)';
 const mainElement = document.getElementById('main');
 mainElement.style.transition = 'background 5s';
 
 const darkenMainBg = () => mainElement.style.background = '#000';
+const lightenMainBg = () => mainElement.style.background = defaultBg;
 
-const lightenMainBg = () => mainElement.style.background = getHSL(defaultArr);
+
+// Event handler
+function onPlayerStateChange(e) {
+	const state = e.data;
+	switch(state) {
+		case 1: //playing
+		case 3: //buffering
+			darkenMainBg();
+			break;
+		case 0: //ended
+		case 2: //paused
+			lightenMainBg();
+			break;
+	}
+}
+
+
+// Create player object
+let player;
+const iframeId = 'player';
+const playerParams = {
+	events: { 'onStateChange': onPlayerStateChange }
+};
+
+function onYouTubeIframeAPIReady() {
+	player = new YT.Player(iframeId, playerParams);
+}
