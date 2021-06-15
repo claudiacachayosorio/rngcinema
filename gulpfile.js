8749c04f16b831d282fd56146d23da2fe7ba0cff
Claudia <claudiacachayosorio.web@gmail.com>
2021-06-15 13:12:10 -0400

    dist published to bucket/src and function zipped and published to bucket

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,53 @@
-const { src, dest, watch, series, parallel } = require('gulp');
-const zip = require('gulp-zip');
+const { src, dest, watch, series } = require('gulp');
 
-const lambdaSrc = './app/src/*';
-function zipTask() {
-	return src(lambdaSrc)
+// Dependencies
+const zip			= require('gulp-zip')
+	  rename		= require('gulp-rename')
+	  awspublish	= require('gulp-awspublish');
+
+// Paths
+const distSrc	= './dist/**/*'
+	  appSrc	= './app/src/**/*'
+	  zipFile	= './app/function.zip';
+
+// S3 publisher
+const s3options = {
+	params: {
+		Bucket: 'rngcinema'
+	}
+};
+const publisher = awspublish.create(s3options);
+
+function publishDist() {
+	return src(distSrc)
+		.pipe(rename(path => path.dirname = '/src/' + path.dirname))
+		.pipe(publisher.publish())
+		.pipe(awspublish.reporter());
+}
+
+function zipFunction() {
+	return src(appSrc)
 		.pipe(zip('function.zip'))
 		.pipe(dest('./app'));
 }
 
+function publishFunction() {
+	return src(zipFile)
+		.pipe(publisher.publish())
+		.pipe(awspublish.reporter());
+}
+
+const lambdaSeries = series(zipFunction, publishFunction);
+
 function watchTask() {
-	watch(lambdaSrc, zipTask);
+	watch(distSrc, publishDist);
+	watch(appSrc, lambdaSeries);
 }
 
 const defaultTask = series(
-	zipTask,
-	watchTask
+	publishDist,
+	lambdaSeries,
+	//watchTask
 );
 
 exports.default = defaultTask;
